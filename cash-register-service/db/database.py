21d493199bd98f9ead4c870fccb26d80acab03f7
Oren Cohen <orenco96@gmail.com>
2025-11-27 13:05:26 +0200

    - fix

--- cash-register-service/db/database.py
+++ cash-register-service/db/database.py
@@ -1,6 +1,7 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.exc import ProgrammingError, OperationalError, IntegrityError
 
 
 DATABASE_URL = os.getenv(
@@ -16,7 +17,15 @@ Base = declarative_base()
 
 def init_db():
     from . import models
-    Base.metadata.create_all(bind=engine)
+    try:
+        Base.metadata.create_all(bind=engine, checkfirst=True)
+    except (ProgrammingError, OperationalError, IntegrityError) as e:
+        # If there is a race condition, the other service will retry and succeed.
+        error_str = str(e).lower()
+        if any(keyword in error_str for keyword in ["already exists", "duplicate key", "pg_type_typname"]):
+            pass
+        else:
+            raise
 
 def get_db():
     db = SessionLocal()
